97ce9d608275635c73b90e09b264dc1c612174da
prachi-max <prachi.nawale09@gmail.com>
2026-04-30 18:13:15 +0530

    change the path

--- src/api.py
+++ src/api.py
@@ -98,7 +98,7 @@ def get_engine_sequence(engine_id: int):
 # 🚀 API ENDPOINT
 # ==============================
 
-@app.get("https://sentinel-smart-system-4.onrender.com/api/v1/health-check/{engine_id}")
+@app.get("/api/v1/health-check/{engine_id}")
 async def get_engine_status(engine_id: int):
     try:
         input_data, current_cycle = get_engine_sequence(engine_id)
